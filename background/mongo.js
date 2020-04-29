53592bae4818208193ba4e0f8031350708658814
eratt <37194876+FastidioCane@users.noreply.github.com>
2020-04-29 17:23:21 +0200

    Added admin mode with corrispective admin schema on DB

--- background/mongo.js
+++ background/mongo.js
@@ -13,3 +13,23 @@ function Setup() {
     var db = mongoose.connection
     db.on('error', console.error.bind(console, 'connection error:'))
 }
+
+var adminSchema = new mongoose.Schema({
+    idAdmin: { type: Number, unique: true, required: true }
+})
+
+var adminModel = mongoose.model('admin', adminSchema, 'admins')
+
+async function GetAdmin(id) {
+    let admin = await adminModel.findOne({
+        idAdmin: id
+    }).lean()
+    return admin
+}
+
+async function SetAdmin(id) {
+    var x = new adminModel({
+        idAdmin: id
+    })
+    await x.save();
+}
--- interface/telegram.js
+++ interface/telegram.js
@@ -28,3 +28,76 @@ bot.onText(/\/start/, async(msg) => {
     });
 
 });
+
+//////////////////////////// ADMIN AUTHORIZATION ///////////////////////////////
+
+bot.onText(/\/admin/, async(msg) => {
+    pattone = await mongo.GetPath(msg.chat.id)
+    pattone = 10
+    await mongo.UpdatePath(msg.chat.id, pattone)
+    var admin = await mongo.GetAdmin(msg.chat.id)
+    if (admin) {
+        bot.sendMessage(msg.chat.id, "You're in!", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{
+                        text: 'Insert New Image',
+                        callback_data: 'ini'
+                    }, {
+                        text: 'Check Votes',
+                        callback_data: 'cv'
+                    }],
+                    [{
+                        text: 'Share Results',
+                        callback_data: 'sr'
+                    }]
+                ]
+            }
+        })
+    } else {
+        bot.sendMessage(msg.chat.id, "You're not authorized!", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{
+                        text: 'Add me to admin list',
+                        callback_data: 'amtal'
+                    }, {
+                        text: 'Main Menu',
+                        callback_data: 'mm'
+                    }]
+                ]
+            }
+        })
+
+    }
+});
+
+bot.on('message', async(msg) => {
+    var pw = "rat"
+    if ((pattone == 10) && (msg.text.toString().includes(pw))) {
+        await mongo.SetAdmin(msg.chat.id)
+        bot.sendMessage(msg.chat.id, 'Now you are in Admin Mode!', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{
+                        text: 'Insert New Image',
+                        callback_data: 'ini'
+                    }, {
+                        text: 'Check Votes',
+                        callback_data: 'cv'
+                    }],
+                    [{
+                        text: 'Share Results',
+                        callback_data: 'sr'
+                    }]
+                ]
+            }
+        })
+    }
+
+    // } else {
+    //     bot.sendMessage(msg.chat.id, 'ERROR: Wrong Password!')
+    //     bot.sendMessage(msg.chat.id, 'Insert Password')
+    // }
+
+})
